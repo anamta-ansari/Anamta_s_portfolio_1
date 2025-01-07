389a89123bc0c59f1b7050c7ebf59071697c755e
anamta-ansari <161108965+anamta-ansari@users.noreply.github.com>
2025-01-08 03:22:29 +0500

    Update Footer.tsx

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,10 +1,3 @@
-// Footer.tsx
-
-import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-
-export default function Footer() {// Footer.tsx
-
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
